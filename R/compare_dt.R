0aaa994fa78454bdc7f698bc0db922eb52a7401c
atrcodebase <39668413+Fahim-Ahmad@users.noreply.github.com>
2022-06-12 01:28:15 +0430

    Update 'compate_dt()' function

--- R/compare_dt.R
+++ R/compare_dt.R
@@ -45,12 +45,13 @@ compare_dt <- function(df1, df2, unique_id_df1, unique_id_df2, compare_all = TRU
     df2 <- df2[, colnames(df2) %in% colnames(df1)]
   }
 
-  if ("KEY" %in% colnames(df1) & unique_id_df1 != "KEY") {
+  if ("KEY" %in% colnames(df1) && unique_id_df1 != "KEY") {
     df1 <- df1 %>%
       rename(key = KEY)
   }
 
   df1 <- df1 %>%
+    # unite('KEY', all_of(unique_id_df1) , remove = TRUE, sep = "_&_") %>%
     select(KEY = all_of(unique_id_df1), everything()) %>%
     mutate(across(-KEY, function(x)
       x = as.character(x)
@@ -58,12 +59,13 @@ compare_dt <- function(df1, df2, unique_id_df1, unique_id_df2, compare_all = TRU
     pivot_longer(-KEY, values_to = "value_1") %>%
     mutate(value_1 = str_squish(value_1))
 
-  if ("KEY" %in% colnames(df2) & unique_id_df2 != "KEY") {
+  if ("KEY" %in% colnames(df2) && unique_id_df2 != "KEY") {
     df2 <- df2 %>%
       rename(key = KEY)
   }
 
   df2 <- df2 %>%
+    # unite('KEY', all_of(unique_id_df2), remove = TRUE, sep = "_&_") %>%
     select(KEY = all_of(unique_id_df2), everything()) %>%
     mutate(across(-KEY, function(x)
       x = as.character(x)
@@ -77,6 +79,15 @@ compare_dt <- function(df1, df2, unique_id_df1, unique_id_df2, compare_all = TRU
     filter((value_1 != value_2) | (is.na(value_1) & !is.na(value_2)) | (!is.na(value_1) & is.na(value_2))) %>%
     rename(question = name, old_value = value_1, new_value = value_2) %>%
     mutate(question = ifelse(question == "key", "KEY", question))
+
+  # if ("question" %in% unique_id_df1) {
+  #   diff <- diff %>% rename(column_name = name, old_value = value_1, new_value = value_2) %>%
+  #     mutate(column_name = ifelse(column_name == "key", "KEY", column_name))
+  # } else {
+  #   diff <- diff %>% rename(question = name, old_value = value_1, new_value = value_2) %>%
+  #     mutate(question = ifelse(question == "key", "KEY", question))
+  # }
+  # diff <- diff %>% separate(KEY, into = unique_id_df1, sep = "_&_")
 
   if(nrow(diff) == 0) {
     paste0("No difference in df1 and df2")
